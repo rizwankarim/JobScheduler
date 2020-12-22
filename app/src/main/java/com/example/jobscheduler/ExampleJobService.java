255ed54cada089b7d2422b2ab306617e16fd990c
Amin Tahseen <51921201+AminTahseen@users.noreply.github.com>
2020-12-22 15:19:36 +0500

    branches merged

--- app/src/main/java/com/example/jobscheduler/ExampleJobService.java
+++ app/src/main/java/com/example/jobscheduler/ExampleJobService.java
@@ -1,11 +1,32 @@
 package com.example.jobscheduler;
 
+import android.Manifest;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Build;
+import android.os.CountDownTimer;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -13,6 +34,39 @@ public class ExampleJobService extends JobService {
 
     private static final String TAG="ExampleJobService";
     private boolean jobCancelled=false;
+    static CountDownTimer countDownTimer = null;
+    place details;
+
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+
+            try
+            {
+                if (locationResult != null && locationResult.getLastLocation() != null) {
+                    double longitude =  locationResult.getLastLocation().getLongitude();
+                    double latitude = locationResult.getLastLocation().getLatitude();
+                    Log.d ("LOCATION_UPDATE",latitude+","+longitude);
+                    details= getGeocodingDetails(longitude,latitude);
+                    // getDetailsFromAPI(latitude+","+longitude,"AIzaSyDazjxsJFdohTwZllHdMsacB4P9luVjqyE");
+                    if(details.getPlaceAddress()!=null)
+                    {
+                        Toast.makeText(ExampleJobService.this, "Address found", Toast.LENGTH_SHORT).show();
+                        //storeDataInDatabase(details,latitude,longitude);
+
+                    }else
+                    {
+                        Toast.makeText(ExampleJobService.this, "Address not found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }catch (Exception er)
+            {
+                Log.d("onLocationResult",er.getMessage());
+            }
+
+        }
+    };
 
     @Override
     public boolean onStartJob(JobParameters params) {
@@ -25,6 +79,7 @@ public class ExampleJobService extends JobService {
     public boolean onStopJob(JobParameters params) {
         Log.d(TAG,"Service has been cancelled..");
         jobCancelled=true;
+        stopLocation();
         return true;
     }
 
@@ -34,50 +89,170 @@ public class ExampleJobService extends JobService {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void run() {
-                for(int i=0; i  < 10;  i++)
-                {
-                    try{
-                        Log.d(TAG,"Count="+i);
-                        //geocding latlong latlng1
-                        //latlng
-                        Double lat1=0.2;
-                        Double lon1=0.1;
+                //Timer();
+                for (int i = 0; i <3 ; i++) {
+                    try {
+                        Log.d(TAG, Integer.toString(i));
+                        LocationRequest locationRequest = new LocationRequest();
 
-                        Double lat2=0.2;
-                        Double lon2=0.1;
+                        // Try adjusting the location.setFastestInterval
+                        /*
+                        50*100 = 5,000 = 5 Seconds
+                        100*100 = 10,000 = 10 Seconds
+                        100*400 = 40,000 = 40 Seconds
+                        100*600 = 60,000 = 60 Seconds
+                        100*900 = 90,000 = 90 Seconds
 
-                       // Double distance=haversineMethod(lat1,lon1,lat2,lon2);
+                         */
+                        locationRequest.setInterval(5000);
+                        locationRequest.setFastestInterval(7000);
+                        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-                        //haversine method(latlng1 latlng2)
-
-                        //if(distance>5){
-                        // false
-                        // }
-                        //else
-                        // {
-                        // count++
-                        // if count 5 {
-                        // google wala
-                        // database wala
-                        // }
-                        // }
-
-                        //count
+                        if (ActivityCompat.checkSelfPermission(ExampleJobService.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ExampleJobService.this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        LocationServices.getFusedLocationProviderClient(ExampleJobService.this)
+                                .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
 
                         if(jobCancelled){
                             return;
                         }
-                        Thread.sleep(5000);
-                    }
-                    catch (Exception e){
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
+//               for(int i=0; i  < 10;  i++){
+//                    try{
+//                        Log.d(TAG,"Service Running...");
+//                        //geocding latlong latlng1
+//                        //latlng
+//                        //haversine method(latlng1 latlng2)
+//                        //if(distance>5){
+//                        // false}
+//                        //else(count++){
+//                        // if count 5 {
+//                        // google wala
+//                        // database wala}}
+//
+//                        //count
+//                        if(jobCancelled){
+//                            return;
+//                        }
+//                        Thread.sleep(5000);
+//                    }
+//                    catch (Exception e){
+//                        e.printStackTrace();
+//                    }
+//                }
                 Log.d(TAG,"Job Finished");
                 jobFinished(params,false);
 
             }
         }).start();
+    }
+
+    public void Timer(){
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        // 60*30*1000 = 30 min
+        // 60*10*1000 = 10 min
+        // 50000 = 50 seconds
+        // 10000 = 10 seconds
+        // 60000 = 60 seconds
+
+        // Try Increasing countDownInterval
+        countDownTimer = new CountDownTimer( 60*10*1000 , 1000) {
+            public void onTick(long millisUntilFinished)
+            {
+                String left=Long.toString(millisUntilFinished);
+                Log.d("Service Time Interval ",left);
+
+                LocationRequest locationRequest = new LocationRequest();
+
+                // Try adjusting the location.setFastestInterval
+                /*
+                50*100 = 5,000 = 5 Seconds
+                100*100 = 10,000 = 10 Seconds
+                100*400 = 40,000 = 40 Seconds
+                100*600 = 60,000 = 60 Seconds
+                100*900 = 90,000 = 90 Seconds
+
+                 */
+                locationRequest.setInterval(5000);
+                locationRequest.setFastestInterval(100*600);
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+                if (ActivityCompat.checkSelfPermission(ExampleJobService.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ExampleJobService.this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                LocationServices.getFusedLocationProviderClient(ExampleJobService.this)
+                        .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+            }
+            public void onFinish()
+            {
+                Toast.makeText(ExampleJobService.this, "Service Has Ended..", Toast.LENGTH_SHORT).show();
+                Log.d("done!", "done!");
+                stopLocation();
+            }
+        };
+        countDownTimer.start();
+    }
+
+    public void stopLocation(){
+        LocationServices.getFusedLocationProviderClient(this)
+                .removeLocationUpdates(locationCallback);
+        stopForeground(true);
+        stopSelf();
+        //countDownTimer.cancel();
+
+    }
+
+    private place getGeocodingDetails(double Longitude, double Latitude) {
+        Geocoder geocoder;
+        place completeDetails = null;
+        List<Address> addresses = new ArrayList<>();
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(Latitude, Longitude, 1);
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+            String knownName = addresses.get(0).getFeatureName();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Date date = new Date();
+            String time = formatter.format(date).toString();
+
+            completeDetails = new place("GuzFS0EjtBSwuRXBuRfhFN8ZSfm1", Latitude, Longitude, address, "pending", time);
+            Log.d("LOCATION_DETAILS", completeDetails.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return completeDetails;
     }
     public double haversineMethod(Double lat1, Double lon1,
                                   Double lat2, Double lon2)

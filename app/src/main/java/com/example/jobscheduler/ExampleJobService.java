e66a95da3125c7f61e68c300c0c824e8be9ffd80
rizwankarim <rizusamnani@gmail.com>
2020-12-22 16:33:47 +0500

    Forloop removed

--- app/src/main/java/com/example/jobscheduler/ExampleJobService.java
+++ app/src/main/java/com/example/jobscheduler/ExampleJobService.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -89,71 +90,30 @@ public class ExampleJobService extends JobService {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void run() {
-                //Timer();
-                for (int i = 0; i <3 ; i++) {
-                    try {
-                        Log.d(TAG, Integer.toString(i));
-                        LocationRequest locationRequest = new LocationRequest();
+               //for(int i=0; i  < 10;  i++){
+                    try{
+                        Log.d(TAG,"Service Running...");
+                        getCurrentLocation();
+                        //geocding latlong latlng1
+                        //latlng
+                        //haversine method(latlng1 latlng2)
+                        //if(distance>5){
+                        // false}
+                        //else(count++){
+                        // if count 5 {
+                        // google wala
+                        // database wala}}
 
-                        // Try adjusting the location.setFastestInterval
-                        /*
-                        50*100 = 5,000 = 5 Seconds
-                        100*100 = 10,000 = 10 Seconds
-                        100*400 = 40,000 = 40 Seconds
-                        100*600 = 60,000 = 60 Seconds
-                        100*900 = 90,000 = 90 Seconds
-
-                         */
-                        locationRequest.setInterval(5000);
-                        locationRequest.setFastestInterval(7000);
-                        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-                        if (ActivityCompat.checkSelfPermission(ExampleJobService.this, Manifest.permission.ACCESS_FINE_LOCATION)
-                                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ExampleJobService.this,
-                                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                            // TODO: Consider calling
-                            //    ActivityCompat#requestPermissions
-                            // here to request the missing permissions, and then overriding
-                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            //                                          int[] grantResults)
-                            // to handle the case where the user grants the permission. See the documentation
-                            // for ActivityCompat#requestPermissions for more details.
-                            return;
-                        }
-                        LocationServices.getFusedLocationProviderClient(ExampleJobService.this)
-                                .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
-
+                        //count
                         if(jobCancelled){
                             return;
                         }
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
+                        Thread.sleep(5000);
+                    }
+                    catch (Exception e){
                         e.printStackTrace();
                     }
-                }
-//               for(int i=0; i  < 10;  i++){
-//                    try{
-//                        Log.d(TAG,"Service Running...");
-//                        //geocding latlong latlng1
-//                        //latlng
-//                        //haversine method(latlng1 latlng2)
-//                        //if(distance>5){
-//                        // false}
-//                        //else(count++){
-//                        // if count 5 {
-//                        // google wala
-//                        // database wala}}
-//
-//                        //count
-//                        if(jobCancelled){
-//                            return;
-//                        }
-//                        Thread.sleep(5000);
-//                    }
-//                    catch (Exception e){
-//                        e.printStackTrace();
-//                    }
-//                }
+                //}
                 Log.d(TAG,"Job Finished");
                 jobFinished(params,false);
 
@@ -161,61 +121,31 @@ public class ExampleJobService extends JobService {
         }).start();
     }
 
-    public void Timer(){
-        if (countDownTimer != null) {
-            countDownTimer.cancel();
+    private void getCurrentLocation() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
         }
-        // 60*30*1000 = 30 min
-        // 60*10*1000 = 10 min
-        // 50000 = 50 seconds
-        // 10000 = 10 seconds
-        // 60000 = 60 seconds
-
-        // Try Increasing countDownInterval
-        countDownTimer = new CountDownTimer( 60*10*1000 , 1000) {
-            public void onTick(long millisUntilFinished)
-            {
-                String left=Long.toString(millisUntilFinished);
-                Log.d("Service Time Interval ",left);
-
-                LocationRequest locationRequest = new LocationRequest();
-
-                // Try adjusting the location.setFastestInterval
-                /*
-                50*100 = 5,000 = 5 Seconds
-                100*100 = 10,000 = 10 Seconds
-                100*400 = 40,000 = 40 Seconds
-                100*600 = 60,000 = 60 Seconds
-                100*900 = 90,000 = 90 Seconds
-
-                 */
-                locationRequest.setInterval(5000);
-                locationRequest.setFastestInterval(100*600);
-                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-                if (ActivityCompat.checkSelfPermission(ExampleJobService.this, Manifest.permission.ACCESS_FINE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ExampleJobService.this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                LocationServices.getFusedLocationProviderClient(ExampleJobService.this)
-                        .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
-            }
-            public void onFinish()
-            {
-                Toast.makeText(ExampleJobService.this, "Service Has Ended..", Toast.LENGTH_SHORT).show();
-                Log.d("done!", "done!");
-                stopLocation();
-            }
-        };
-        countDownTimer.start();
+        LocationServices.getFusedLocationProviderClient(ExampleJobService.this)
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(ExampleJobService.this)
+                                .removeLocationUpdates(this);
+                        if (locationResult != null && locationResult.getLocations().size() > 0) {
+                            int locationindex = locationResult.getLocations().size() - 1;
+                            double current_lat=locationResult.getLocations().get(locationindex).getLatitude();
+                            double current_long=locationResult.getLocations().get(locationindex).getLongitude();
+                            //double current_lat = 24.863473;
+                            //double current_long = 67.072290;
+                            Log.d("Location", String.valueOf(current_lat) + "," + String.valueOf(current_long));
+                        }
+                    }
+                }, Looper.getMainLooper());
     }
 
     public void stopLocation(){
@@ -276,5 +206,9 @@ public class ExampleJobService extends JobService {
         double c = 2 * Math.asin(Math.sqrt(a));
 
         return rad * c;
+    }
+
+    public void GetCurrentTime(){
+        Date currentTime= Calendar.getInstance().getTime();
     }
 }

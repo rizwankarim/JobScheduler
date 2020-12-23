a9dacf6122df310720818bf37f92d0f9cf260b4f
rizwankarim <rizusamnani@gmail.com>
2020-12-23 11:18:49 +0500

    Geocoding Updated

--- app/src/main/java/com/example/jobscheduler/ExampleJobService.java
+++ app/src/main/java/com/example/jobscheduler/ExampleJobService.java
@@ -33,7 +33,8 @@ import java.util.Locale;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class ExampleJobService extends JobService {
-
+    Geocoder geocoder;
+    place completeDetails=null;
 
     @Override
     public boolean onStartJob(JobParameters params) {
@@ -43,5 +44,31 @@ public class ExampleJobService extends JobService {
     @Override
     public boolean onStopJob(JobParameters params) {
         return false;
+    }
+
+    private place getGeocodingDetails(double Longitude, double Latitude){
+        List<Address> addresses= new ArrayList<>();
+        geocoder=new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses= geocoder.getFromLocation(Latitude,Longitude,1);
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+            String knownName = addresses.get(0).getFeatureName();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Date date = new Date();
+            String time=formatter.format(date).toString();
+
+            completeDetails= new place("GuzFS0EjtBSwuRXBuRfhFN8ZSfm1",Latitude,Longitude,address,"pending",time);
+            Log.d("LOCATION_DETAILS",completeDetails.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return completeDetails;
     }
 }
--- app/src/main/java/com/example/jobscheduler/MainActivity.java
+++ app/src/main/java/com/example/jobscheduler/MainActivity.java
@@ -22,8 +22,7 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG="MainActivity";
-    Geocoder geocoder;
-    place completeDetails=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,29 +41,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private place getGeocodingDetails(double Longitude, double Latitude){
-        List<Address> addresses= new ArrayList<>();
-        geocoder=new Geocoder(this, Locale.getDefault());
-
-        try {
-            addresses= geocoder.getFromLocation(Latitude,Longitude,1);
-            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-            String city = addresses.get(0).getLocality();
-            String state = addresses.get(0).getAdminArea();
-            String country = addresses.get(0).getCountryName();
-            String postalCode = addresses.get(0).getPostalCode();
-            String knownName = addresses.get(0).getFeatureName();
-
-            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-            Date date = new Date();
-            String time=formatter.format(date).toString();
-
-            completeDetails= new place("GuzFS0EjtBSwuRXBuRfhFN8ZSfm1",Latitude,Longitude,address,"pending",time);
-            Log.d("LOCATION_DETAILS",completeDetails.toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return completeDetails;
-    }
 }
